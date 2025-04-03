01d930fc8cb53726c5e82ffc976e50bb41cb567c
João Távora <joaotavora@gmail.com>
2025-04-03 22:55:47 +0100

    Use Google's RE2 library

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include "logger.hpp"
 
+#include <re2/re2.h>
+
+#include <array>
 #include <iostream>
 #include <istream>
 #include <optional>
-#include <regex>
 #include <unordered_map>
 #include <unordered_set>
 #include <list>
@@ -13,6 +15,7 @@ using line_t = std::string;
 using input_t = std::list<line_t>;
 using label_t = std::string;
 using linum_t = size_t;
+using matches_t = std::array<std::string, 10>;
 
 auto slurp(std::istream& is) {
   input_t v;
@@ -32,10 +35,25 @@ struct user_options {
   bool preserve_unused_labels{false};
 };
 
-void sweeping(input_t& input, const user_options& o, auto fn) {
-  for (auto it = input.begin();;) {
-    std::smatch matches;
+namespace utils {
+template<typename Dest, typename T, size_t N, std::size_t... Is>
+constexpr auto make_pointer_array_impl(std::array<T, N>& values, std::index_sequence<Is...>) {
+  return std::array<Dest, N>{&values[Is]...};
+}
 
+template<typename Dest, typename T, size_t N>
+constexpr auto make_pointer_array(std::array<T, N>& values) {
+  return make_pointer_array_impl<Dest>(values, std::make_index_sequence<N>{});
+}
+} // namespace utils
+
+void sweeping(input_t& input, const user_options& o, auto fn) {
+
+  matches_t  matches;
+  auto match_ptrs = utils::make_pointer_array<const RE2::Arg>(matches);
+  auto arg_ptrs = utils::make_pointer_array<const RE2::Arg* const>(match_ptrs);
+
+  for (auto it = input.begin();;) {
     bool done{false};
     size_t linum{0};
 
@@ -52,8 +70,10 @@ void sweeping(input_t& input, const user_options& o, auto fn) {
     };
 
     auto match = [&](auto&& re,
-                     const std::smatch** out_matches, int from = 0) -> bool {
-      if (std::regex_search(it->cbegin()+from, it->cend(), matches, re)) {
+                     const matches_t** out_matches, int from = 0) -> bool {
+      std::string_view a(it->cbegin() + from, it->cend());
+      if (RE2::PartialMatchN(a, re, &arg_ptrs.at(1), re.NumberOfCapturingGroups())) {
+        matches[0] = a;
         if (out_matches) *out_matches = &matches;
         return true;
       } else
@@ -73,20 +93,18 @@ void sweeping(input_t& input, const user_options& o, auto fn) {
   }
 }
 
-
-
 // clang-format off
-const std::regex r_label_start                {R"(^([^:]+): *(?:#|$)(?:.*))"};
-const std::regex r_has_opcode                 {R"(^[[:space:]]+[A-Za-z]+)"};
-const std::regex r_comment_only               {R"(^[[:space:]]*(?:[#;@]|//|/\*.*\*/).*$)"};
-const std::regex r_label_reference            {R"(\.[A-Z_a-z][$.0-9A-Z_a-z]*)"};
-const std::regex r_defines_global             {R"(^[[:space:]]*\.globa?l[[:space:]]*([.A-Z_a-z][$.0-9A-Z_a-z]*))"};
-const std::regex r_defines_function_or_object {R"(^[[:space:]]*\.type[[:space:]]*(.*),[[:space:]]*[%@])"};
-const std::regex r_source_file_hint           {R"(^[[:space:]]*\.file[[:space:]]+([[:digit:]]+)[[:space:]]+\"([^\"]+)\"(?:[[:space:]]+\"([^\"]+)\")?.*)"};
-const std::regex r_source_tag                 {R"(^[[:space:]]*\.loc[[:space:]]+([[:digit:]]+)[[:space:]]+([[:digit:]]+).*)"};
-const std::regex r_source_stab                {R"(^.*\.stabn[[:space:]]+([[:digit:]]+),0,([[:digit:]]+),.*)"};
-const std::regex r_endblock                   {R"(\.(?:cfi_endproc|data|section|text))"};
-const std::regex r_data_defn                  {R"(^[[:space:]]*\.(string|asciz|ascii|[1248]?byte|short|word|long|quad|value|zero))"};
+const RE2 r_label_start                {R"(^([^:]+): *(?:#|$)(?:.*))"};
+const RE2 r_has_opcode                 {R"(^[[:space:]]+[A-Za-z]+)"};
+const RE2 r_comment_only               {R"(^[[:space:]]*(?:[#;@]|//|/\*.*\*/).*$)"};
+const RE2 r_label_reference            {R"(\.[A-Z_a-z][$.0-9A-Z_a-z]*)"};
+const RE2 r_defines_global             {R"(^[[:space:]]*\.globa?l[[:space:]]*([.A-Z_a-z][$.0-9A-Z_a-z]*))"};
+const RE2 r_defines_function_or_object {R"(^[[:space:]]*\.type[[:space:]]*(.*),[[:space:]]*[%@])"};
+const RE2 r_source_file_hint           {R"(^[[:space:]]*\.file[[:space:]]+([[:digit:]]+)[[:space:]]+\"([^\"]+)\"(?:[[:space:]]+\"([^\"]+)\")?.*)"};
+const RE2 r_source_tag                 {R"(^[[:space:]]*\.loc[[:space:]]+([[:digit:]]+)[[:space:]]+([[:digit:]]+).*)"};
+const RE2 r_source_stab                {R"(^.*\.stabn[[:space:]]+([[:digit:]]+),0,([[:digit:]]+),.*)"};
+const RE2 r_endblock                   {R"(\.(?:cfi_endproc|data|section|text))"};
+const RE2 r_data_defn                  {R"(^[[:space:]]*\.(string|asciz|ascii|[1248]?byte|short|word|long|quad|value|zero))"};
 // clang-format on
 
 struct parser_state {
@@ -107,17 +125,16 @@ void first_pass(
     input_t& input, const std::string& main_file_name, parser_state& s,
     const user_options& o) {
 
-
+  const matches_t* matches{};
   sweeping(input, o, [&](auto preserve, auto kill, auto match_1, auto it, auto) {
-    const std::smatch* matches{};
 
     auto match = [&](auto&& re, int from = 0) { return match_1(re, &matches, from); };
     if (it->at(0) != '\t') {
       if ((match(r_label_start))) {
         LOG_TRACE("FP1.1 '{}'", *it);
-        if (s.routines.contains(matches->str(1))) {
+        if (s.routines.contains(matches->at(1))) {
           LOG_TRACE("FP1.1.1 '{}'", *it);
-          s.current_routine = matches->str(1);
+          s.current_routine = matches->at(1);
         }
         LOG_TRACE("Preserve: FP1.1 '{}'", *it);
         preserve();
@@ -128,11 +145,11 @@ void first_pass(
     } else {
       if (s.current_routine && match(r_has_opcode)) {
         LOG_TRACE("FP2.1 '{}'", *it);
-        auto offset = matches->length(0);
+        auto offset = matches->at(0).size();
         while (match(r_label_reference, offset)) {
           LOG_TRACE("FP2.1.1 '{}'", *it);
-          s.routines[*s.current_routine].push_back(matches->str(0));
-          offset += matches->length(0);
+          s.routines[*s.current_routine].push_back(matches->at(0));
+          offset += matches->at(0).size();
         }
         LOG_TRACE("Preserve: FP2.1 '{}'", *it);
         preserve();
@@ -142,17 +159,17 @@ void first_pass(
       } else if (
                  match(r_defines_global) || match(r_defines_function_or_object)) {
         LOG_TRACE("FP2.3 '{}'", *it);
-        s.routines[matches->str(1)] = {};
+        s.routines[matches->at(1)] = {};
       } else if (
           match(r_source_file_hint) &&
           main_file_name ==
-            (matches->length(3) ? matches->str(3) : matches->str(2))) {
+            (matches->at(3).size() ? matches->at(3) : matches->at(2))) {
         LOG_TRACE("FP2.4 '{}'", *it);
-        s.main_file_tag = matches->str(1);
+        s.main_file_tag = matches->at(1);
       } else if (match(r_source_tag)) {
         LOG_TRACE("FP2.5 '{}'", *it);
         if (s.current_routine && s.main_file_tag &&
-          matches->str(1) == s.main_file_tag) {
+          matches->at(1) == s.main_file_tag) {
           LOG_TRACE("FP2.5.1 '{}'", *it);
           s.main_file_routines.insert(*s.current_routine);
         }
@@ -190,14 +207,14 @@ void second_pass(input_t& input, parser_state& s, const user_options& o) {
   std::optional<size_t> source_linum{};
 
   sweeping(input, o, [&](auto preserve, auto kill, auto match_1, auto it, auto asm_linum) {
-    const std::smatch* matches{};
+    const matches_t* matches{};
 
     auto match = [&](auto&& re) { return match_1(re, &matches); };
 
     if (it->at(0) != '\t') {
       if ((match(r_label_start))) {
         LOG_TRACE("SP1.1 '{}'", *it);
-        label_t l = matches->str(1);
+        label_t l = matches->at(1);
         if (s.used_labels.contains(l)) {
           reachable_label = l;
           LOG_TRACE("Preserve: SP1.1.1 '{}'", *it);
