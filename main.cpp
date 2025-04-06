a1052c91f32ffd288c07f9f1b5d64ac6efc98cde
João Távora <joaotavora@gmail.com>
2025-04-06 17:15:37 +0100

    Clean things up before adding JSON output

--- main.cpp
+++ main.cpp
@@ -1,6 +1,3 @@
-#include "logger.hpp"
-#include "linespan.hpp"
-
 #include <re2/re2.h>
 
 #include <array>
@@ -8,10 +5,15 @@
 #include <iostream>
 #include <iterator>
 #include <optional>
+#include <print>
 #include <stdexcept>
 #include <unordered_map>
 #include <unordered_set>
-#include <print>
+
+#include "linespan.hpp"
+#include "logger.hpp"
+
+namespace xpto {
 
 using input_t = xpto::linespan;
 using match_t = std::string_view;
@@ -28,12 +30,13 @@ struct user_options {
 };
 
 namespace utils {
-template<typename Dest, typename T, size_t N, std::size_t... Is>
-constexpr auto make_pointer_array_impl(std::array<T, N>& values, std::index_sequence<Is...>) {
+template <typename Dest, typename T, size_t N, std::size_t... Is>
+constexpr auto make_pointer_array_impl(
+    std::array<T, N>& values, std::index_sequence<Is...>) {
   return std::array<Dest, N>{&values[Is]...};
 }
 
-template<typename Dest, typename T, size_t N>
+template <typename Dest, typename T, size_t N>
 constexpr auto make_pointer_array(std::array<T, N>& values) {
   return make_pointer_array_impl<Dest>(values, std::make_index_sequence<N>{});
 }
@@ -47,12 +50,10 @@ std::optional<size_t> to_size_t(std::string_view sv) {
     return std::nullopt;
 }
 
-} // namespace utils
+}  // namespace utils
 
 template <typename Output, typename Input, typename F>
-[[nodiscard]] Output sweeping(const Input& input, const user_options& o, F fn) {
-
-  Output retval{};
+void sweeping(const Input& input, Output& output, const user_options& o, F fn) {
   size_t linum{1};
 
   std::array<match_t, 10> matches;
@@ -64,7 +65,7 @@ template <typename Output, typename Input, typename F>
 
     auto preserve = [&]() {
       linum++;
-      retval.push_back(*it);
+      output.push_back(*it);
       ++it;
       done = true;
     };
@@ -76,7 +77,8 @@ template <typename Output, typename Input, typename F>
 
     auto match = [&](auto&& re, matches_t& out_matches, int from = 0) -> bool {
       match_t a(it->cbegin() + from, it->cend());
-      if (RE2::PartialMatchN(a, re, &arg_ptrs.at(1), re.NumberOfCapturingGroups())) {
+      if (RE2::PartialMatchN(
+              a, re, &arg_ptrs.at(1), re.NumberOfCapturingGroups())) {
         matches[0] = a;
         out_matches = matches_t(matches.begin(), re.NumberOfCapturingGroups());
         return true;
@@ -89,13 +91,18 @@ template <typename Output, typename Input, typename F>
       LOG_TRACE("EOF");
       break;
     }
-    if (!it->size()) { kill(); continue; }
+    if (!it->size()) {
+      kill();
+      continue;
+    }
     fn(preserve, kill, match, it, asm_linum);
     if (!done) {
-      if (o.preserve_directives) preserve(); else kill();
+      if (o.preserve_directives)
+        preserve();
+      else
+        kill();
     }
   }
-  return retval;
 }
 
 // clang-format off
@@ -128,71 +135,75 @@ struct parser_state {
 
 auto first_pass(
     const auto& input, const std::string& main_file_name, parser_state& s,
-    const user_options& o) {
-
-  using output_t = std::vector<typename std::decay_t<decltype(input)>::value_type>;
+    const user_options& options) {
+  using output_t =
+      std::vector<typename std::decay_t<decltype(input)>::value_type>;
+  output_t output;
 
   matches_t matches{};
-  return sweeping<output_t>(input, o, [&](auto preserve, auto kill, auto match_1, auto it, auto) {
-
-    auto match = [&](auto&& re, int from = 0) { return match_1(re, matches, from); };
-    if (it->at(0) != '\t') {
-      if ((match(r_label_start))) {
-        LOG_TRACE("FP1.1 '{}'", *it);
-        if (s.routines.contains(matches[1])) {
-          LOG_TRACE("FP1.1.1 '{}'", *it);
-          s.current_routine = matches[1];
+  sweeping<output_t>(
+      input, output, options,
+      [&](auto preserve, auto kill, auto match_1, auto it, auto) {
+        auto match = [&](auto&& re, int from = 0) {
+          return match_1(re, matches, from);
+        };
+        if (it->at(0) != '\t') {
+          if ((match(r_label_start))) {
+            LOG_TRACE("FP1.1 '{}'", *it);
+            if (s.routines.contains(matches[1])) {
+              LOG_TRACE("FP1.1.1 '{}'", *it);
+              s.current_routine = matches[1];
+            }
+            LOG_TRACE("Preserve: FP1.1 '{}'", *it);
+            preserve();
+          } else {
+            LOG_TRACE("Kill: FP1.1 '{}'", *it);
+            kill();
+          }
+        } else {
+          if (s.current_routine && match(r_has_opcode)) {
+            LOG_TRACE("FP2.1 '{}'", *it);
+            auto offset = matches[0].size();
+            while (match(r_label_reference, offset)) {
+              LOG_TRACE("FP2.1.1 '{}'", *it);
+              s.routines[*s.current_routine].push_back(matches[0]);
+              offset += matches[0].size();
+            }
+            LOG_TRACE("Preserve: FP2.1 '{}'", *it);
+            preserve();
+          } else if (!options.preserve_comments && match(r_comment_only)) {
+            LOG_TRACE("Kill: FP2.2 '{}'", *it);
+            kill();
+          } else if (
+              match(r_defines_global) || match(r_defines_function_or_object)) {
+            LOG_TRACE("FP2.3 '{}'", *it);
+            s.routines[matches[1]] = {};
+          } else if (
+              match(r_source_file_hint) &&
+              main_file_name == (matches[3].size() ? matches[3] : matches[2])) {
+            LOG_TRACE("FP2.4 '{}'", *it);
+            s.main_file_tag = matches[1];
+          } else if (match(r_source_tag)) {
+            LOG_TRACE("FP2.5 '{}'", *it);
+            if (s.current_routine && s.main_file_tag &&
+                matches[1] == s.main_file_tag) {
+              LOG_TRACE("FP2.5.1 '{}'", *it);
+              s.main_file_routines.insert(*s.current_routine);
+            }
+            LOG_TRACE("Preserve: FP2.5 '{}'", *it);
+            preserve();
+          } else if (match(r_endblock)) {
+            LOG_TRACE("FP2.6 '{}'", *it);
+            s.current_routine = std::nullopt;
+            LOG_TRACE("Preserve: FP2.6 '{}'", *it);
+            preserve();
+          } else {
+            LOG_TRACE("Preserve: FP2.7 '{}'", *it);
+            preserve();
+          }
         }
-        LOG_TRACE("Preserve: FP1.1 '{}'", *it);
-        preserve();
-      } else {
-        LOG_TRACE("Kill: FP1.1 '{}'", *it);
-        kill();
-      }
-    } else {
-      if (s.current_routine && match(r_has_opcode)) {
-        LOG_TRACE("FP2.1 '{}'", *it);
-        auto offset = matches[0].size();
-        while (match(r_label_reference, offset)) {
-          LOG_TRACE("FP2.1.1 '{}'", *it);
-          s.routines[*s.current_routine].push_back(matches[0]);
-          offset += matches[0].size();
-        }
-        LOG_TRACE("Preserve: FP2.1 '{}'", *it);
-        preserve();
-      } else if (!o.preserve_comments && match(r_comment_only)) {
-        LOG_TRACE("Kill: FP2.2 '{}'", *it);
-        kill();
-      } else if (
-                 match(r_defines_global) || match(r_defines_function_or_object)) {
-        LOG_TRACE("FP2.3 '{}'", *it);
-        s.routines[matches[1]] = {};
-      } else if (
-          match(r_source_file_hint) &&
-          main_file_name ==
-            (matches[3].size() ? matches[3] : matches[2])) {
-        LOG_TRACE("FP2.4 '{}'", *it);
-        s.main_file_tag = matches[1];
-      } else if (match(r_source_tag)) {
-        LOG_TRACE("FP2.5 '{}'", *it);
-        if (s.current_routine && s.main_file_tag &&
-          matches[1] == s.main_file_tag) {
-          LOG_TRACE("FP2.5.1 '{}'", *it);
-          s.main_file_routines.insert(*s.current_routine);
-        }
-        LOG_TRACE("Preserve: FP2.5 '{}'", *it);
-        preserve();
-      } else if (match(r_endblock)) {
-        LOG_TRACE("FP2.6 '{}'", *it);
-        s.current_routine = std::nullopt;
-        LOG_TRACE("Preserve: FP2.6 '{}'", *it);
-        preserve();
-      } else {
-        LOG_TRACE("Preserve: FP2.7 '{}'", *it);
-        preserve();
-      }
-    }
-  });
+      });
+  return output;
 }
 
 void intermediate(parser_state& s, const user_options& o) {
@@ -201,107 +212,116 @@ void intermediate(parser_state& s, const user_options& o) {
   if (o.preserve_library_functions) {
     for (auto&& [label, callees] : s.routines) {
       s.used_labels.insert(label);
-      for (auto&& callee : callees) { s.used_labels.insert(callee); }
+      for (auto&& callee : callees) {
+        s.used_labels.insert(callee);
+      }
     }
   } else {
     for (auto&& label : s.main_file_routines) {
       s.used_labels.insert(label);
-      for (auto&& callee : s.routines[label]) { s.used_labels.insert(callee); }
+      for (auto&& callee : s.routines[label]) {
+        s.used_labels.insert(callee);
+      }
     }
   }
 }
 
-auto second_pass(const auto& input, parser_state& s, const user_options& o) {
+auto second_pass(
+    const auto& input, parser_state& s, const user_options& options) {
   std::optional<label_t> reachable_label{};
   std::optional<size_t> source_linum{};
 
-  using output_t = std::vector<typename std::decay_t<decltype(input)>::value_type>;
+  using output_t =
+      std::vector<typename std::decay_t<decltype(input)>::value_type>;
+  output_t output;
 
-  return sweeping<output_t>(input, o, [&](auto preserve, auto kill, auto match_1, auto it, auto asm_linum) {
+  sweeping(
+      input, output, options,
+      [&](auto preserve, auto kill, auto match_1, auto it, auto asm_linum) {
+        matches_t matches{};
+        auto match = [&](auto&& re) { return match_1(re, matches); };
 
-    matches_t matches{};
-    auto match = [&](auto&& re) { return match_1(re, matches); };
-
-    if (it->at(0) != '\t') {
-      if ((match(r_label_start))) {
-        LOG_TRACE("SP1.1 '{}'", *it);
-        label_t l = matches[1];
-        if (s.used_labels.contains(l)) {
-          reachable_label = l;
-          LOG_TRACE("Preserve: SP1.1.1 '{}'", *it);
-          preserve();
-        } else if (o.preserve_unused_labels) {
-          LOG_TRACE("Preserve: SP1.1.2 '{}'", *it);
-          preserve();
+        if (it->at(0) != '\t') {
+          if ((match(r_label_start))) {
+            LOG_TRACE("SP1.1 '{}'", *it);
+            label_t l = matches[1];
+            if (s.used_labels.contains(l)) {
+              reachable_label = l;
+              LOG_TRACE("Preserve: SP1.1.1 '{}'", *it);
+              preserve();
+            } else if (options.preserve_unused_labels) {
+              LOG_TRACE("Preserve: SP1.1.2 '{}'", *it);
+              preserve();
+            } else {
+              LOG_TRACE("Kill: SP1.1.3 '{}'", *it);
+              kill();
+            }
+          }
         } else {
-          LOG_TRACE("Kill: SP1.1.3 '{}'", *it);
-          kill();
-        }
-      }
-    } else {
-      if (match(r_data_defn) && reachable_label) {
-        LOG_TRACE("Preserve: SP2.1 '{}'", *it);
-        preserve();
-      } else if (match(r_has_opcode) && reachable_label) {
-        if (source_linum) {
-          s.register_mapping(*source_linum, asm_linum());
-          LOG_TRACE("SP2.2.1 '{}'", *it);
-        }
-        LOG_TRACE("Preserve: SP2.2 '{}'", *it);
-        preserve();
-      } else if (match(r_source_tag)) {
-        LOG_TRACE("SP2.3 '{}'", *it);
-        source_linum = [&]() -> std::optional<int> {
-          if (*s.main_file_tag == matches[1]) {
-            return utils::to_size_t(matches[2]);
-          } else {
-            return std::nullopt;
+          if (match(r_data_defn) && reachable_label) {
+            LOG_TRACE("Preserve: SP2.1 '{}'", *it);
+            preserve();
+          } else if (match(r_has_opcode) && reachable_label) {
+            if (source_linum) {
+              s.register_mapping(*source_linum, asm_linum());
+              LOG_TRACE("SP2.2.1 '{}'", *it);
+            }
+            LOG_TRACE("Preserve: SP2.2 '{}'", *it);
+            preserve();
+          } else if (match(r_source_tag)) {
+            LOG_TRACE("SP2.3 '{}'", *it);
+            source_linum = [&]() -> std::optional<int> {
+              if (*s.main_file_tag == matches[1]) {
+                return utils::to_size_t(matches[2]);
+              } else {
+                return std::nullopt;
+              }
+            }();
+          } else if (match(r_source_stab)) {
+            LOG_TRACE("SP2.4 '{}'", *it);
+            // http://www.math.utah.edu/docs/info/stabs_11.html
+            // 68     0x44     N_SLINE   line number in text segment
+            // 100    0x64     N_SO      path and name of source file
+            // 132    0x84     N_SOL     Name of sub-source (#include) file.
+            auto a = utils::to_size_t(matches[1]);
+            if (a) {
+              switch (a.value()) {
+                case 68:
+                  source_linum = utils::to_size_t(matches[2]);
+                  break;
+                case 100:
+                case 132:
+                  source_linum = std::nullopt;
+                  break;
+                default: {
+                }
+              }
+            }
+          } else if (match(r_endblock)) {
+            LOG_TRACE("SP2.5 '{}'", *it);
+            reachable_label = std::nullopt;
           }
-        }();
-      } else if (match(r_source_stab)) {
-        LOG_TRACE("SP2.4 '{}'", *it);
-        // http://www.math.utah.edu/docs/info/stabs_11.html
-        // 68     0x44     N_SLINE   line number in text segment
-        // 100    0x64     N_SO      path and name of source file 
-        // 132    0x84     N_SOL     Name of sub-source (#include) file.
-        auto a = utils::to_size_t(matches[1]);
-        if (a) {
-          switch (a.value()) {
-          case 68:
-            source_linum = utils::to_size_t(matches[2]);
-            break;
-          case 100:
-          case 132:
-            source_linum = std::nullopt;
-            break;
-          default: {}
-          }
         }
-      } else if (match(r_endblock)) {
-        LOG_TRACE("SP2.5 '{}'", *it);
-        reachable_label = std::nullopt;
-      }
-    }
-  });
+      });
+  return output;
 }
+}  // namespace xpto
 
 int main() {
-  try {
-  std::vector<char> buf{std::istreambuf_iterator<char>{std::cin},
-                        std::istreambuf_iterator<char>()};
-  // auto input = slurp(std::cin);
+  xpto::logger::set_level(xpto::logger::level::debug);
+  std::vector<char> buf{
+    std::istreambuf_iterator<char>{std::cin}, std::istreambuf_iterator<char>()};
   auto input = xpto::linespan{buf};
 
-  xpto::logger::set_level(xpto::logger::level::debug);
+  xpto::parser_state state{};
+  xpto::user_options options{};
 
-  parser_state state{};
-  user_options options{};
+  try {
+    auto fp_output = first_pass(input, "<stdin>", state, options);
+    intermediate(state, options);
+    auto sp_output = second_pass(fp_output, state, options);
 
-  auto fp_output = first_pass(input, "<stdin>", state, options);
-  intermediate(state, options);
-  auto sp_output = second_pass(fp_output, state, options);
-
-  for (auto&& l : sp_output) std::cout << l << "\n";
+    for (auto&& l : sp_output) std::cout << l << "\n";
 
   } catch (std::exception& e) {
     LOG_FATAL("Whoops {}", e.what());
