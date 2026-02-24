83aa1d0f8ed586d3f5fcf60ae2ad70bd5e2e9de9
João Távora <joaotavora@gmail.com>
2026-02-24 12:26:22 +0000

    Make test/fixture/gcc-deep-hierarchy-2 a bit more complex
    * test/fixture/gcc-deep-hierarchy-2/source.cpp: Tweak.
    
    * test/fixture/gcc-deep-hierarchy-2/inner/header.hpp: Tweak.
    
    * test/fixture/gcc-deep-hierarchy-2/inner/hmmm.hpp: New file.

--- test/fixture/gcc-deep-hierarchy-2/source.cpp
+++ test/fixture/gcc-deep-hierarchy-2/source.cpp
@@ -1,5 +1,16 @@
 #include "header.hpp"
 #include "inner/header.hpp"
+#include "inner/hmmm.hpp"
 
 int use_outer() { return outer_fn(); }
 int use_inner() { return inner_fn(); }
+
+int hmmm_test(const char* coiso) {
+  hmmm test{std::span(coiso, coiso + 1000)};
+
+  auto retval = 0;
+  for (auto l : test) {
+    retval++;
+  }
+  return 42 + retval;
+}
